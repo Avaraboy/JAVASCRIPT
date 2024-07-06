b6aa6c49fd2bcb3f2e0af3a2b9ba66a931b086ca
Avaraboy <avaraboypc@gmail.com>
2024-07-06 21:42:23 +0530

    new code

--- first.js
+++ first.js
@@ -1,7 +1,20 @@
 // console.log("Start learning")
 // console.log("This is first js page")
 
-fname= "Dharmendra Singh";
-age = "20";
-clas = "4th sem";
-console.log("Name -",fname,"Age -",age);
+// fname= "Dharmendra Singh";
+// age = "20";
+// clas = "4th sem";
+// console.log("Name -",fname,"Age -",age);
+
+const student = {
+    name : "Dharmendra Singh", 
+    age : 20,
+    branch : "AIDS",
+    year : "2nd",
+};
+
+student["year"]= "3rd";
+
+console.log(student);
+console.log(student["name"]);
+console.log(student["year"]);
