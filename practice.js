1757ddbfbad642bed5b8b1beb397a6e2bd64c662
Avaraboy <avaraboypc@gmail.com>
2024-07-06 22:15:26 +0530

    new file

---
+++ practice.js
@@ -0,0 +1,11 @@
+const profile = {
+    userid : "avara.boy.989",
+    username : "Dharmendra Singh",
+    isfollow : true,
+    posts : 195,
+    followers : 569,
+    following : 4,
+    bio : "student of b.tech",
+};
+
+console.log(profile);
