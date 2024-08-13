6aaa0faab9321d9c82c3842c4749a810f9bb9509
Avaraboy <avaraboypc@gmail.com>
2024-08-14 00:27:35 +0530

    conditional Statements

---
+++ third.js
@@ -0,0 +1,27 @@
+// Conditional statement 
+
+// if statement
+let age = 18;
+
+if (age >= 18){
+    console.log("You are able to vote");       
+}
+
+
+if (age < 18){
+    console.log("You are not able to vote");       
+}
+
+// if-else statement
+
+let mode = "blue";
+let color;
+
+if(mode=="dark"){
+    color="black";
+}
+else{
+    color="white"
+}
+
+console.log(color);
