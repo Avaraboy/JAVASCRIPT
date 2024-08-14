05ece9d50dd647986d50ecee262ca139bddf3c69
Avaraboy <avaraboypc@gmail.com>
2024-08-14 11:28:41 +0530

    conditional statementes

--- third.js
+++ third.js
@@ -32,4 +32,12 @@ let ag = 20;
 
 if(ag<18){console.log("you are junior");}
 else if(ag>60){console.log("you are senior");}
-else{console.log("you are in midle")}
+else{console.log("you are in middle")}
+
+
+// Ternary Operater ( simple form of if-else)
+
+// let result = ag >= 18 ? "you are adult" : "you are not adult";
+// console.log(result);
+
+ag >= 18 ? console.log("you are adult") : console.log("you are not adult");      // both are same but we prefer previous code.
