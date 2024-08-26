8de0416738698f43a40a00b13675a55a12ecfb88
Avaraboy <avaraboypc@gmail.com>
2024-08-26 11:02:55 +0530

    practice

--- seven practice.js
+++ seven practice.js
@@ -9,16 +9,26 @@ function countvowels(str) {
     console.log("The number of vowels present in string is",count);
 }
 
-countvowels("Dharmendra");   // output The number of vowels present in string is 3
+countvowels("DhAarmendra");   // output The number of vowels present in string is 3
 
 // using arrrow function
+let coun =0;
 
 const countvowel = (string) =>{for(const c of string){
-    if (char==="a"||char==="e"||char==="i"||char==="o"||char==="u") {
-        count++;
+    if (c==="a"||c==="e"||c==="i"||c==="o"||c==="u") {
+        coun++;
     }
 }
-console.log("The number of vowels present in string is",count);
+console.log("The number of vowels present in string is",coun);
 }
 
-countvowel("a");   // output The number of vowels present in string is 1
+countvowel("abcd");   // output The number of vowels present in string is 1
+
+
+// For each loop
+let a = [2,3,4,5,6,7,8];
+console.log("The square of the array is")
+a.forEach((val) => {
+    sq=val ** 2;
+    console.log(sq);
+});
